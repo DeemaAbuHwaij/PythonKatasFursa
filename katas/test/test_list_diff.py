b7012ac5890c0d17570eb9e755883161d7d51967
deema <demaabohwej@gmail.com>
2025-04-22 19:19:22 +0300

    list_diff solution

---
+++ katas/test/test_list_diff.py
@@ -0,0 +1,19 @@
+import unittest
+from katas.list_diff import find_difference
+
+
+class MyTestCase(unittest.TestCase):
+    def tset1(self):
+        self.assertEqual(find_difference([10, 3, 5, 6, 20, -2]), 22)  # add assertion here
+    def test2(self):
+        self.assertEqual(find_difference([0, 3, 1, 6, 10, -20]), 30)  # add assertion here
+    def test3(self):
+        self.assertEqual(find_difference([10, 30, 5, 6, 20, 1]), 29)  # add assertion here
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
